519449fed905dcc8dc926b26d1f87f9f310f97b4
Karthik-webdev <87525742+Karthik-webdev@users.noreply.github.com>
2021-07-17 04:21:05 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './App.css';
 import Header from './components/header';
 import Home from './components/Home';
+import Section from './components/Section';
 
 function App() {
   return (
